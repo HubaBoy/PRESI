796a4291534b4a034af801356cd3772c5de73510
HubaBoy <101318913+HubaBoy@users.noreply.github.com>
2024-04-27 21:51:09 +0300

    fixing score bugs

--- src/src.ino
+++ src/src.ino
@@ -1,77 +1,81 @@
 #include <MD_MAX72xx.h>
 #include <SPI.h>
 
-#define PRINT(s, v) { Serial.print(F(s)); Serial.print(v); }
-
+#define PRINT(s, v) \
+  {                 \
+    Serial.print(F(s)); \
+    Serial.print(v);    \
+  }
 
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
 #define MAX_DEVICES 4
 
-#define CLK_PIN   13  // or SCK
-#define DATA_PIN  11  // or MOSI
-#define CS_PIN    10  // or SS
+#define CLK_PIN 13 // or SCK
+#define DATA_PIN 11 // or MOSI
+#define CS_PIN 10   // or SS
 
 MD_MAX72XX mx = MD_MAX72XX(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 
+#define CHAR_SPACING 1
 
-#define CHAR_SPACING  1 
-
-#define BUF_SIZE  75
-char *message = "HIT IT";
+#define BUF_SIZE 75
+char message[BUF_SIZE] = "HIT IT";
 int IRPin = 2;
 int butPin = 4;
-float minTime = 2;
+float minTime = 0.2;
 float time = 0;
-int score;
+int score =0;
+bool isHit = false;
+
 void printText(uint8_t modStart, uint8_t modEnd, char *pMsg)
 {
-  uint8_t   state = 0;
-  uint8_t   curLen;
-  uint16_t  showLen;
-  uint8_t   cBuf[8];
-  int16_t   col = ((modEnd + 1) * COL_SIZE) - 1;
+  uint8_t state = 0;
+  uint8_t curLen;
+  uint16_t showLen;
+  uint8_t cBuf[8];
+  int16_t col = ((modEnd + 1) * COL_SIZE) - 1;
 
   mx.control(modStart, modEnd, MD_MAX72XX::UPDATE, MD_MAX72XX::OFF);
 
-  do  
+  do
   {
-    switch(state)
+    switch (state)
     {
-      case 0:
+    case 0:
       if (*pMsg == '\0')
-        {
-          showLen = col - (modEnd * COL_SIZE); 
-          state = 2;
-          break;
-        }
-
-        showLen = mx.getChar(*pMsg++, sizeof(cBuf)/sizeof(cBuf[0]), cBuf);
-        curLen = 0;
-        state++;
-       
-      case 1: 
-        mx.setColumn(col--, cBuf[curLen++]);
-        if (curLen == showLen)
-        {
-          showLen = CHAR_SPACING;
-          state = 2;
-        }
+      {
+        showLen = col - (modEnd * COL_SIZE);
+        state = 2;
         break;
+      }
 
-      case 2:
-        curLen = 0;
-        state++;
-        
-      case 3:	
-       mx.setColumn(col--, 0);
-        curLen++;
-        if (curLen == showLen)
-          state = 0;
-        break;
+      showLen = mx.getChar(*pMsg++, sizeof(cBuf) / sizeof(cBuf[0]), cBuf);
+      curLen = 0;
+      state++;
 
-      default:
-        col = -1;     
-        }
+    case 1:
+      mx.setColumn(col--, cBuf[curLen++]);
+      if (curLen == showLen)
+      {
+        showLen = CHAR_SPACING;
+        state = 2;
+      }
+      break;
+
+    case 2:
+      curLen = 0;
+      state++;
+
+    case 3:
+      mx.setColumn(col--, 0);
+      curLen++;
+      if (curLen == showLen)
+        state = 0;
+      break;
+
+    default:
+      col = -1;
+    }
   } while (col >= (modStart * COL_SIZE));
 
   mx.control(modStart, modEnd, MD_MAX72XX::UPDATE, MD_MAX72XX::ON);
@@ -83,29 +87,41 @@ void setup()
   Serial.begin(9600);
   pinMode(IRPin, INPUT);
   pinMode(butPin, INPUT);
-  printText(0, MAX_DEVICES-1, message);
+  printText(0, MAX_DEVICES - 1, message);
 }
 
 void loop()
 {
-   delay(1000);
-  if(digitalRead(IRPin)==0)
+  delay(1000);
+  if (digitalRead(IRPin) == 0)
   {
+    if (isHit)
+      isHit = false;
+
     Serial.println("Punch the reflex back");
-  }else{
-     Serial.println("The reflex back is puncked");
-     time = 0;
-    while(digitalRead(butPin))
+  }
+  else
+  {
+    if (!isHit)
     {
-      delay(1);
-      time+=0.001;
+      Serial.println("The reflex back is punched");
+      time = 0;
+      while (digitalRead(butPin))
+      {
+        delay(1);
+        time += 0.001;
+      }
+      Serial.println(time);
+      score = (time > 0) ? (minTime / time) * 1000 : 0;
+      if (score > 1000 || score < 0 || time<0|| score == 0)
+        score = 999;
+      sprintf(message, "%d", score);
+      Serial.println(message);
+      Serial.print("That's your score: ");
+      Serial.println(score);
+      int len = strlen(message);
+      printText(0, MAX_DEVICES - 1, message);
+      isHit = true; // Set isHit to true only after processing the hit
     }
-  score = (minTime/time)*1000;
-  if (score> 1000) score = 999;
-    sprintf(message, "%d", score);
-    Serial.println(message);
-    Serial.print("thats your score");
-    int len = strlen(message);
-   printText(0, MAX_DEVICES-1, message);
   }
 }
